3b9b230aec3b54656aa45b51b9c326aba7f6b962
unknown <d.moradisabzevar@gmail.com>
2023-01-31 12:28:58 +0330

    Added FPS counter

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
+import time
+
 import cv2
 import mediapipe as mp
 import os
 
-USE_CASCADE = False
-USE_LANDMARK = True  #Uses mediapipe mobile net
+USE_CASCADE = False  # Uses classic haar solution
+USE_LANDMARK = True  # Uses mediapipe mobile net
+DISPLAY_TIME = 1  # Updates FPS. sec
 
 mp_drawing = mp.solutions.drawing_utils
 
@@ -13,16 +16,20 @@ mp_face = mp.solutions.face_detection.FaceDetection(
 )
 
 cv2_base_dir = os.path.dirname(os.path.abspath(cv2.__file__))
-print(cv2_base_dir)
 haar_model = os.path.join(cv2_base_dir, 'data/haarcascade_frontalface_default.xml')
 
 faceCascade = cv2.CascadeClassifier(haar_model)
 
+prev_time = time.time()
+
 video_capture = cv2.VideoCapture(0)
 ret = True
+frame_counter = 0
+disp = "FPS: "
+
 while ret:
     ret, frame = video_capture.read()
-
+    frame_counter += 1
     if USE_CASCADE:
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         faces = faceCascade.detectMultiScale(
@@ -34,8 +41,7 @@ while ret:
         )
 
         for (x, y, w, h) in faces:
-            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
     elif USE_LANDMARK:
         image_input = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
@@ -46,6 +52,14 @@ while ret:
             for detection in results.detections:
                 mp_drawing.draw_detection(frame, detection)
 
+    time_dif = time.time() - prev_time
+    if time_dif >= DISPLAY_TIME:
+        fps = frame_counter/time_dif
+        frame_counter = 0
+        disp = "FPS: "+str(fps)[:5]
+        prev_time = time.time()
+
+    cv2.putText(frame, disp, (10, 25), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (0, 255, 0), 1)
     cv2.imshow('Video', frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
